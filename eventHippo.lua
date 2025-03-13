e8901e9b1846ab17bbaf3152cdf362b49e2ca62a
Kingltnn <108787320+Kingltnn@users.noreply.github.com>
2025-03-13 16:23:15 +0700

    Update eventHippo.lua

--- eventHippo.lua
+++ eventHippo.lua
@@ -26,6 +26,8 @@ getgenv().Configuration = {
         ['Upgrades'] = { "LuckyRaidPets", "LuckyRaidDamage", "LuckyRaidAttackSpeed" },
     },
     ['Equip Enchants'] = {"Strong Pets", "Criticals", "Strong Pets", "Criticals", "Strong Pets", "Strong Pets"},
+    ['Debug'] = {}
 }
+
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b0dec084ff36c27d9dc119ad6b094544.lua"))()
  
