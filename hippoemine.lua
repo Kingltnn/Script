4958d470deb580036fa7b44592d99e011e0206ef
Kingltnn <108787320+Kingltnn@users.noreply.github.com>
2025-03-28 06:58:25 +0700

    Create hippoemine.lua

---
+++ hippoemine.lua
@@ -0,0 +1,25 @@
+getgenv().Configuration = { -- This script does NOT hatch eggs ONLY digsite
+    ['Auto Mail'] = {
+        ['Items'] = {
+            ['Currency'] = {
+                ['Diamonds'] = { ['Amount'] = 50000000 },
+            },
+            ['Misc'] = {
+                ['Ruby Gem'] = { ['Amount'] = 20000 },
+                ['Sapphire Gem'] = { ['Amount'] = 150000 },
+                ['Emerald Gem'] = { ['Amount'] = 1000 },
+                ['Amethyst Gem'] = { ['Amount'] = 300 },
+                ['Rainbow Gem'] = { ['Amount'] = 10 },
+            },
+        },
+        ['Mail All Huges'] = false,
+        ['Usernames'] = {"letunamrb"},
+    },
+    ['Event'] = {
+        ['Max Break Time'] = 60, -- Priority Bypasses
+        ['Priority Blocks'] = {"Ruby","Emerald", "Amethyst", "Rainbow"},
+    },
+    ['Consumables'] = { ["Mining Damage Booster"] = 25, ["Mining Speed Booster"] = 25,["Mining Coin Booster"] = 25, },
+    ['Debug'] = {true, true, false, true}, -- Opt, Menu, Print, Block Transparency
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/06b65bbac4b00bb34141dfc01d201acd.lua"))()
