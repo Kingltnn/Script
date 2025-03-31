9def25c4d2ec71e3916461362522206c61fcb8cd
Kingltnn <108787320+Kingltnn@users.noreply.github.com>
2025-03-31 18:20:59 +0700

    Update and rename rank to rank.lua

--- rank.lua
+++ rank.lua
@@ -15,6 +15,6 @@ getgenv().Config = {
 
     ["FPSBooster"] = true, -- REPORT NO SCRIPT BUGS.
     ["UltraFPS"] = true, -- I WILL BAN YOU IF YOU REPORT ANY SCRIPT BUGS.
-    ["Webhook"] = "", -- DO NOT REPORT ANY SCRIPT BUGS. WE KNOW ITS BUGGED.
+    ["Webhook"] = "https://discord.com/api/webhooks/1337237444306927701/B8bBd7j6GFdPrR-uAR644DdiMTl1CxW_yTkLU3ia2_5XWBAf0ZULFLTAwDlIGATarDHY", -- DO NOT REPORT ANY SCRIPT BUGS. WE KNOW ITS BUGGED.
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/177d65e916988f4b84dd21ed33a2aa91.lua"))()
