f295dcf2c3a2cffa4443143750b399314bed9fad
Kingltnn <108787320+Kingltnn@users.noreply.github.com>
2025-04-15 10:34:27 +0700

    Create BGSI.lua

---
+++ BGSI.lua
@@ -0,0 +1,48 @@
+repeat task.wait() until game:IsLoaded() and game.Players.LocalPlayer
+getgenv().Config = {
+    ["AutoBlowBubble"] = true,
+    ["AutoSellBubble"] = true,
+    ["AutoBuyGum"] = true,
+    ["AutoHatchEggs"] = true,
+    ["AutoUnlockIslands"] = true,
+    ["AutoEquipBestPets"] = true,
+    ["AutoClaimRewards"] = true,
+    ["AutoUpgradeMasteryAndClaim"] = false,
+    ["AutoClaimSeason"] = true,
+    ["AutoHatchSelectedEgg"] = false,
+    ["AutoRiftEggLuckHatch"] = true,
+    ["AutoCheckInventoryFull"] = true,
+    ["TURNOFF3DRENDER"] = false,
+    ["UsePotions"] = true,
+    ["SelectedEgg"] = "Common Egg",
+    ["ESpamDuration"] = 3,
+    ["MaxHatchAttempts"] = 100,
+    ["IslandUnlockAttempts"] = 2,
+    ["SetFPS"] = 60,
+    ["SetDefaultEggToHatch"] = false,
+
+    ["WebhookPet"] = {
+        ["PingUserId"] = "",
+        ["WebhookURL"] = "https://discord.com/api/webhooks/1335784018360406046/q0NywBiu-N48kYqYEwOUt8ZOdTVOTVQy25QhFGn9ugiIpgFOAdNkfKYlg5jtOMLKGYBk",
+        ["WebhookRaritySender"] = {
+            ["Common"] = false,
+            ["Unique"] = false,
+            ["Rare"] = false,
+            ["Epic"] = false,
+            ["Legendary"] = true,
+            ["Mythic"] = true,
+            ["Secret"] = true
+        }
+    },
+
+    ["AutoDeleteRarityList"] = {
+        ["Common"] = true,
+        ["Unique"] = true,
+        ["Rare"] = true,
+        ["Epic"] = true,
+        ["Legendary"] = false,
+        ["Secret"] = false
+    }
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/StrenTheBeginner/asenranhroi/refs/heads/main/Kaitun-BGSI-V2"))()
