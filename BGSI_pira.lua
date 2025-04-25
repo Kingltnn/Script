0a45192ae9221298d1a7b6e8a3c8a0bd833bdb68
Kingltnn <108787320+Kingltnn@users.noreply.github.com>
2025-04-25 16:16:32 +0700

    Create BGSI_pira.lua

---
+++ BGSI_pira.lua
@@ -0,0 +1,15 @@
+getgenv().Config = {
+    ["Hatching"] = {
+        ["Rifts"] = {
+            ["Eggs"] = {"Aura Egg", "Throwback Egg", "Pastel Egg", "Bunny Egg", "Nightmare Egg"}
+        },
+        ["Egg"] = "Pastel Egg" -- // Doesnt work yet, but will in the future
+    },
+    ["Webhook"] = {
+        ["MinDifficulty"] = "1m",
+        ["UserID"] = "",
+        ["URL"] = "https://discord.com/api/webhooks/1364783387889635419/IQuXU9ql4SQGGQ8PzE36Ez5PT1VVHoDfzRcArXEuYFM5jomS_d1okwzoXOF6o2c2Zmmc",
+    },
+    ["AutoEnchant"] = false -- // Enchant Team Up on all Equipped Pets
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/6a28683e5e681161a5074613f6daf64c.lua"))()
