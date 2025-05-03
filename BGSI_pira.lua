2e8f8af32aac9b5716afed18507ff4cc51879fb1
Kingltnn <108787320+Kingltnn@users.noreply.github.com>
2025-05-03 07:31:55 +0000

    Update BGSI_pira.lua

--- BGSI_pira.lua
+++ BGSI_pira.lua
@@ -1,7 +1,7 @@
 getgenv().Config = {
     ["Hatching"] = {
         ["Rifts"] = {
-            ["Eggs"] = {"Throwback Egg", "Pastel Egg", "Bunny Egg","100M Egg","Nightmare Egg"}
+            ["Eggs"] = {"Throwback Egg", "Pastel Egg", "Bunny Egg","Nightmare Egg"}
         },
         ["Egg"] = "100M Egg" -- // Doesnt work yet, but will in the future
     },
@@ -10,6 +10,9 @@ getgenv().Config = {
         ["UserID"] = "",
         ["URL"] = "https://discord.com/api/webhooks/1364783387889635419/IQuXU9ql4SQGGQ8PzE36Ez5PT1VVHoDfzRcArXEuYFM5jomS_d1okwzoXOF6o2c2Zmmc",
     },
-    ["AutoEnchant"] = true -- // Enchant Team Up on all Equipped Pets
+    ["AutoEnchant"] = true, -- // Enchant Team Up on all Equipped Pets
+       ["Debug"] = {
+        DisableUI = true,
+         },
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/6a28683e5e681161a5074613f6daf64c.lua"))()
